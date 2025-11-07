b8f5177119dfc4e7b5c45163c1ba3df26ea7eb5f
Rutujaaware <rutujabjadhav1999@gmail.com>
2025-11-07 21:44:51 +0530

    Update views.py

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,85 +1,88 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib import messages
 from .models import Category, Product
 from .forms import CategoryForm, ProductForm
 
-# -------------------- CATEGORY CRUD --------------------
 
-def category_list(request):
-    categories = Category.objects.all()
-    return render(request, 'products/category_list.html', {'categories': categories})
+# -------------------- CATEGORY VIEWS --------------------
 
-
-def category_create(request):
-    if request.method == 'POST':
-        form = CategoryForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Category created successfully!")
-            return redirect('category_list')
-    else:
-        form = CategoryForm()
-    return render(request, 'products/category_form.html', {'form': form})
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'products/category_list.html'
+    context_object_name = 'categories'
 
 
-def category_update(request, pk):
-    category = get_object_or_404(Category, pk=pk)
-    if request.method == 'POST':
-        form = CategoryForm(request.POST, request.FILES, instance=category)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Category updated successfully!")
-            return redirect('category_list')
-    else:
-        form = CategoryForm(instance=category)
-    return render(request, 'products/category_form.html', {'form': form})
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'products/category_form.html'
+    success_url = reverse_lazy('category_list')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Category created successfully!")
+        return super().form_valid(form)
 
 
-def category_delete(request, pk):
-    category = get_object_or_404(Category, pk=pk)
-    if request.method == 'POST':
-        category.delete()
-        messages.success(request, "Category deleted successfully!")
-        return redirect('category_list')
-    return render(request, 'products/category_confirm_delete.html', {'category': category})
+class CategoryUpdateView(UpdateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = 'products/category_form.html'
+    success_url = reverse_lazy('category_list')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Category updated successfully!")
+        return super().form_valid(form)
 
 
-# -------------------- PRODUCT CRUD --------------------
+class CategoryDeleteView(DeleteView):
+    model = Category
+    template_name = 'products/category_confirm_delete.html'
+    success_url = reverse_lazy('category_list')
 
-def product_list(request):
-    products = Product.objects.select_related('category').all()
-    return render(request, 'products/product_list.html', {'products': products})
-
-
-def product_create(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Product created successfully!")
-            return redirect('product_list')
-    else:
-        form = ProductForm()
-    return render(request, 'products/product_form.html', {'form': form})
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "Category deleted successfully!")
+        return super().delete(request, *args, **kwargs)
 
 
-def product_update(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES, instance=product)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Product updated successfully!")
-            return redirect('product_list')
-    else:
-        form = ProductForm(instance=product)
-    return render(request, 'products/product_form.html', {'form': form})
+# -------------------- PRODUCT VIEWS --------------------
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+    queryset = Product.objects.select_related('category').all()
 
 
-def product_delete(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    if request.method == 'POST':
-        product.delete()
-        messages.success(request, "Product deleted successfully!")
-        return redirect('product_list')
-    return render(request, 'products/product_confirm_delete.html', {'product': product})
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'products/product_form.html'
+    success_url = reverse_lazy('product_list')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Product created successfully!")
+        return super().form_valid(form)
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'products/product_form.html'
+    success_url = reverse_lazy('product_list')
+
+    def form_valid(self, form):
+        messages.success(self.request, "Product updated successfully!")
+        return super().form_valid(form)
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'products/product_confirm_delete.html'
+    success_url = reverse_lazy('product_list')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "Product deleted successfully!")
+        return super().delete(request, *args, **kwargs)
+
+
